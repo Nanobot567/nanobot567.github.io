1d918539a7d58dbddb02ca79ca424e57127eb689
Nanobot567 <70984367+Nanobot567@users.noreply.github.com>
2023-08-15 19:31:34 +0000

    update stuff

---
+++ js/index.js
@@ -0,0 +1,8 @@
+function hideshow(elem) {
+    var x = document.getElementById(elem);
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
