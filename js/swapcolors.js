c9e496af2441542f6c8e81e726875a00e032ed6d
nanobot567 <567.nanobot@gmail.com>
2025-05-25 15:23:08 -0500

    woah big stuff changes!
    research -> archives, new blog post, more 88x31s, probably some other stuff
    
    
    Former-commit-id: 70ce358018c9f0b0be965a1600061c643ee574b1
    Former-commit-id: e82e507ae6ee08841a9330e717d6080042359481

--- js/swapcolors.js
+++ js/swapcolors.js
@@ -1,15 +1,6 @@
-function hideshow(elem) {
-    var x = document.getElementById(elem);
-    if (x.style.display === "none") {
-        x.style.display = "block";
-    } else {
-        x.style.display = "none";
-    }
-}
-
-const element = document.querySelector('.colorswap');
-
 function swapColors() {
+  const element = document.querySelector('.colorswap');
+
   if (element !== null) {
     const styles = window.getComputedStyle(element);
 
